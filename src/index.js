1f8d73d4869de0368df913b48ab65ce9e652d8cb
sindhuja15673 <ssindhujak69@gmail.com>
2024-08-06 15:41:22 +0530

    third3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +22,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// Register the service worker
+serviceWorkerRegistration.register();
